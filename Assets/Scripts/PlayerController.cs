155a2859ad8db37e36e9b320806cc3b16df5ea1c
LucaSchifano <luca.schifano005@gmail.com>
2022-04-02 10:03:32 +0200

    Scene luca + Player script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float ladderSpeed; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
